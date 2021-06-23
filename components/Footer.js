cf54b355f45782910309849d544b58f40ebefb44
Sally Hoffmann <shoffmann2791@gmail.com>
2021-06-22 22:27:08 -0500

    social media links

--- components/Footer.js
+++ components/Footer.js
@@ -1,9 +1,22 @@
 import html from "html-literal";
 
+import Insta1 from "../Lib/Insta1.png";
+import fb from "../Lib/fb.png";
+import linked from "../Lib/linked.png";
 export default () => html`
   <footer>
     <div class="fixed-footer">
       <div class="container">
+        <a target="_blank" href="https://www.instagram.com/sally.hoff27/">
+          <img src="${Insta1}" height="40px" />
+        </a>
+        <a target="_blank" href="https://www.linkedin.com/in/sallyhoffmann/">
+          <img src="${linked}" height="40px" />
+        </a>
+        <a target="_blank" href="https://www.facebook.com/sally.hoffmann">
+          <img src="${fb}" height="40px" padding="10px" />
+        </a>
+
         Copyright &copy; 2021 Sally Hoffmann Photgraphy
       </div>
     </div>
